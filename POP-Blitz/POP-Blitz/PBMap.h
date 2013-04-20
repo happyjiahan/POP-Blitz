a024a52b7c20cda220f24267bf158c1b82a28a67
happyjiahan <happyjiahan@gmail.com>
2013-04-20 13:15:01 +0800

    add map files

---
+++ POP-Blitz/POP-Blitz/PBMap.h
@@ -0,0 +1,13 @@
+//
+//  PBMap.h
+//  POP-Blitz
+//
+//  Created by lijiahan on 13-4-20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PBMap : NSObject
+
+@end
---
+++ POP-Blitz/POP-Blitz/PBMap.m
@@ -0,0 +1,13 @@
+//
+//  PBMap.m
+//  POP-Blitz
+//
+//  Created by lijiahan on 13-4-20.
+//
+//
+
+#import "PBMap.h"
+
+@implementation PBMap
+
+@end
